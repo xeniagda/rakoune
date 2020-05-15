6cf8bdcb4eb3186e070fa1a0d9cf044d11bebf8b
loovjo <jonathan.loov@gmail.com>
2020-05-15 11:33:23 +0200

    Quad utility function

--- src/render/text/text_gpu_primitives.rs
+++ src/render/text/text_gpu_primitives.rs
@@ -17,6 +17,18 @@ unsafe impl bytemuck::Pod for Vertex {}
 unsafe impl bytemuck::Zeroable for Vertex {}
 
 impl Vertex {
+    pub fn create_quad(xy_0: [f32; 2], xy_1: [f32; 2], uv_0: [f32; 2], uv_1: [f32; 2]) -> [Vertex; 6] {
+        let tl = Vertex { position: [xy_0[0], xy_0[1]], fontdata_uv: [uv_0[0], uv_0[1]]};
+        let tr = Vertex { position: [xy_1[0], xy_0[1]], fontdata_uv: [uv_1[0], uv_0[1]]};
+        let bl = Vertex { position: [xy_0[0], xy_1[1]], fontdata_uv: [uv_0[0], uv_1[1]]};
+        let br = Vertex { position: [xy_1[0], xy_1[1]], fontdata_uv: [uv_1[0], uv_1[1]]};
+
+        [
+            tl, bl, tr,
+            br, tr, bl,
+        ]
+    }
+
     pub fn desc<'a>() -> VertexBufferDescriptor<'a> {
         VertexBufferDescriptor {
             stride: size_of::<Vertex>() as u64,
